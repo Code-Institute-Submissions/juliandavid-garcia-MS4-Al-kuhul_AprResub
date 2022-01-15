aeb0061e8f65ad5f8d00def56bbabc35a8c82457
juliandavid-garcia <juliandavid.garcia@gmail.com>
2022-01-15 01:39:41 +0000

    completed webhook handler

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,7 +2,6 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
-from profiles.models import UserProfile
 
 import json
 import time
@@ -38,21 +37,6 @@ class StripeWH_Handler:
         for field, value in shipping_details.address.items():
             if value == "":
                 shipping_details.address[field] = None
-        
-        # Update profile information if save_info was checked
-        profile = None
-        username = intent.metadata.username
-        if username != 'AnonymousUser':
-            profile = UserProfile.objects.get(user__username=username)
-            if save_info:
-                profile.default_phone_number = shipping_details.phone
-                profile.default_country = shipping_details.address.country
-                profile.default_postcode = shipping_details.address.postal_code
-                profile.default_town_or_city = shipping_details.address.city
-                profile.default_street_address1 = shipping_details.address.line1
-                profile.default_street_address2 = shipping_details.address.line2
-                profile.default_county = shipping_details.address.state
-                profile.save()
 
         order_exists = False
         attempt = 1
@@ -86,7 +70,6 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
-                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
@@ -107,6 +90,7 @@ class StripeWH_Handler:
                             quantity=item_data,
                         )
                         order_line_item.save()
+                        
             except Exception as e:
                 if order:
                     order.delete()
