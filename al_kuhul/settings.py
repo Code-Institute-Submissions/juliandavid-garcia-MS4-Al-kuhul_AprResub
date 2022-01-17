cc853cc54be66b77b3ee4b57cb10c44199f6ec23
juliandavid-garcia <juliandavid.garcia@gmail.com>
2022-01-17 01:46:09 +0000

    tried the heroku run python3 manage.py migrate

--- al_kuhul/settings.py
+++ al_kuhul/settings.py
@@ -133,8 +133,6 @@ else:
             'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
         }
     }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
 
