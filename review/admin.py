d0b405a8e3ebd318679c23572337114a18008d7d
juliandavid-garcia <juliandavid.garcia@gmail.com>
2022-01-22 03:21:35 +0000

    added class ReviewAdmin

--- review/admin.py
+++ review/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
-
+from .models import Product, Category, Order, OrderLineItem, UserProfile, Review,
 # Register your models here.
+@admin.register(Review)
+class ReviewsAdmin(admin.ModelAdmin):
+    list_display = ['id','user_profile','product','rate','created_at']
+    readonly_fields = ['created_at',]
--- review/models.py
+++ review/models.py
@@ -1,4 +1,5 @@
 from django.db import models
+from al_kuhul import Review
 
 # Create your models here.
 
@@ -8,7 +9,7 @@ class Reviews(models.Model):
     product = models.ForeignKey(Product, models.CASCADE)
     comment = models.TextField(max_length=300)
     rate = models.IntegerField(default=0)
-    created_at = models.DateTimeField(auto_now_add = True)
+    created_at = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return str(self.id)
